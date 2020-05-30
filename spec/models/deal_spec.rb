0c887d61ccfa5c9282ca6fc82cbf6a027e1f2923
Redvan <redvanisation@gmail.com>
2020-05-29 20:09:14 -0400

    Add test to check if the the db has been seeded

--- spec/models/deal_spec.rb
+++ spec/models/deal_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Deal, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:value) }
   it { should validate_presence_of(:stage) }
+
+  it 'checks if the deals has been seeded to the database' do
+    expect(Deal.count).not_to eq(0)
+  end
 end
